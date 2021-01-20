26906b410753cf6b01713332092cac5935c7b51d
Thomas <me@hvs.cx>
2021-01-20 21:05:23 +0100

    add margin between getcard

--- src/components/GetCard.js
+++ src/components/GetCard.js
@@ -5,14 +5,16 @@ const FeatureCard = (props) => {
 
     return (
         // lg:w-80
-        <div className="py-3 px-6 w-full flex items-center justify-between shadow-2xl shadow-inner border border-graybell rounded-3xl bg-gradient-to-tl from-graybell via-graybell-light to-graybell bg-opacity-60">
-            <div className="w-2/5">
-                {svg}
-            </div>
-            <div className="w-3/5">
-                <h4 className="text-gray-50 text-xl">
-                    {title}
-                </h4>
+        <div className="mx-2">
+            <div className="w-full py-3 px-6 flex items-center justify-between shadow-2xl shadow-inner border border-graybell rounded-3xl bg-gradient-to-tl from-graybell via-graybell-light to-graybell bg-opacity-60">
+                <div className="w-2/5">
+                    {svg}
+                </div>
+                <div className="w-3/5">
+                    <h4 className="text-gray-50 text-xl">
+                        {title}
+                    </h4>
+                </div>
             </div>
         </div>
     );
