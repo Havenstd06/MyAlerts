1ea777e4c39db8d19651b7a0d4e1c33dfa7dd4b3
Thomas <me@hvs.cx>
2021-01-26 12:43:04 +0100

    Instagram link & align input and button on hero

--- src/components/HeroHome.js
+++ src/components/HeroHome.js
@@ -10,14 +10,14 @@ function HeroHome() {
                         Don't Miss
                     </h1>
                     <h1 className="tracking-wider text-center text-gray-50 md:text-left text-5xl lg:text-6xl" data-aos="zoom-y-out" data-aos-delay="1700">
-                        Releases
+                        Anything
                     </h1>
                     <h1 className="mb-8 tracking-wider text-center text-lightbell md:text-left text-5xl lg:text-6xl" data-aos="zoom-y-out" data-aos-delay="1900">
                         With MyAlerts
                     </h1>
-                    <div className="flex xl:flex-row flex-col items-center xl:items-start justify-center xl:justify-start w-5/6 md:w-3/4 xl:w-full">
+                    <div className="flex flex-row items-center justify-start w-full sm:w-2/3 md:w-full">
                         <input
-                            className="px-4 w-full xl:w-3/5 flex-grow text-center md:text-left py-1 mb-4 mr-2 text-base text-gray-200 bg-transparent border-b-2 border-gray-50 focus:outline-none sm:mb-0"
+                            className="px-4 w-full flex-grow text-left mt-1 py-1 mb-4 mr-2 text-base text-gray-200 bg-transparent border-b-2 border-gray-50 focus:outline-none sm:mb-0"
                             placeholder="Your Phone Number"
                             type="tel"
                             data-aos="zoom-y-out"
@@ -26,7 +26,7 @@ function HeroHome() {
 
                         <a
                             href="https://myalerts.io/subscribe"
-                            className="w-full xl:w-2/5 xl:ml-4 mt-4 xl:mt-0 inline-flex justify-center items-center px-3 py-3.5 border border-transparent rounded-full shadow-sm text-white bg-lightbell-light hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-200 ease-in-out"
+                            className="w-2/3 xl:w-4/5 ml-4 inline-flex justify-center items-center px-3 py-3.5 border border-transparent rounded-full shadow-sm text-white bg-lightbell-light hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-200 ease-in-out"
                             data-aos="zoom-y-out" data-aos-delay="2300"
                         >
                             Join Today
--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -8,16 +8,25 @@ const Footer = (props) => {
             <div className="container flex flex-col flex-wrap justify-between px-5 py-6 mx-auto lg:px-20 sm:flex-row">
                 <span className="inline-flex items-center justify-center mt-2 sm:mt-0 sm:justify-start">
                     {/*Twitter*/}
+                    {/*<a href="#" className="text-bluebell transition duration-200 ease-out hover:text-graybell">*/}
+                    {/*    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"*/}
+                    {/*         className="w-6 h-6" viewBox="0 0 24 24">*/}
+                    {/*        <path*/}
+                    {/*            d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z">*/}
+                    {/*        </path>*/}
+                    {/*    </svg>*/}
+                    {/*</a>*/}
+
+                    {/*Instagram*/}
                     <a href="#" className="text-bluebell transition duration-200 ease-out hover:text-graybell">
-                        <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                             className="w-6 h-6" viewBox="0 0 24 24">
-                            <path
-                                d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z">
-                            </path>
+                        <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                             strokeWidth="2" className="w-6 h-6" viewBox="0 0 24 24">
+                            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"/>
+                            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"/>
                         </svg>
                     </a>
-                    <a href="#" className="ml-4 text-bluebell text-sm font-inter font-medium tracking-wider transition duration-200 ease-out hover:text-graybell">
-                        @ALERTS_IO
+                    <a href="https://www.instagram.com/my.alerts/?igshid=a4y5jvcf9dx0" className="ml-4 text-bluebell text-sm font-inter font-medium tracking-wider transition duration-200 ease-out hover:text-graybell">
+                        @MY.ALERTS
                     </a>
                 </span>
                 <span className="inline-flex items-center justify-center mt-2 sm:mt-0 sm:justify-start">
